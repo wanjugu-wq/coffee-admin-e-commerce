5057729f585c32f89e4e2dee47d828c36038578e
wanjugu-wq <wanjuguzzz@gmail.com>
2026-05-16 23:22:31 +0300

    edited home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,18 +2,30 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="bg-[#f7f3ee] shadow-md px-6 py-4 flex justify-between items-center w-full h-25">
-      <h1 className="text-2xl font-bold text-[#e7730c]">Tavern Coffee</h1>
+    <nav className="bg-[#211c19] shadow-md px-6 py-4 flex justify-between items-center w-full h-25">
+      <h1 className="text-2xl font-bold italic text-stone-100 tracking-wider">
+        Tavern Coffee
+      </h1>
 
       <div className="flex gap-6 text-[#2b1d17]">
         <NavLink
           to="/"
-          className="hover:underline hover:decoration-orange-500 transition-all duration-300"
+          className="hover:underline hover:decoration-orange-500 transition-all duration-1000 text-white font-bold italic "
         >
           Home
         </NavLink>
-        <NavLink to="/products">Products</NavLink>
-        <NavLink to="/add-product">Add Product</NavLink>
+        <NavLink
+          to="/products"
+          className="hover:underline hover:decoration-orange-500 transition-all duration-1000  text-white font-bold italic"
+        >
+          Products
+        </NavLink>
+        <NavLink
+          to="/add-product"
+          className="hover:underline hover:decoration-orange-500 transition-all duration-1000 text-white font-bold italic"
+        >
+          Add Product
+        </NavLink>
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,11 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       {/* HERO SECTION */}
-
       <section className="relative h-[90vh] flex items-center justify-center text-center px-6 overflow-hidden">
         <div className="absolute inset-0"></div>
 
         <div className="relative z-10 max-w-4xl">
-          <p className="uppercase tracking-[0.4em] text-amber-300 mb-4">
+          <p className="uppercase tracking-[0.3em] text-amber-300 mb-4">
             Tavern Inspired Coffee House
           </p>
 
@@ -32,7 +31,7 @@ export default function Home() {
 
             <Link
               to="/add-product"
-              className="border border-amber-400 hover:bg-amber-400 hover:text-black px-8 py-4 rounded-xl font-bold transition"
+              className="border border-amber-400 hover:bg-stone-100 hover:text-black px-8 py-4 rounded-xl font-bold transition"
             >
               Add Product
             </Link>
@@ -41,7 +40,6 @@ export default function Home() {
       </section>
 
       {/* FEATURES SECTION */}
-
       <section className="px-6 py-10 bg-[#211c19]">
         <div className="max-w-6xl mx-auto">
           <div className="text-center mb-16">
@@ -56,12 +54,9 @@ export default function Home() {
 
           <div className="grid md:grid-cols-3 gap-8">
             <div className="bg-[#2a221e] p-8 rounded-2xl hover:-translate-y-2 transition">
-              <div className="text-5xl mb-5"></div>
-
               <h3 className="text-2xl font-bold text-amber-400 mb-4 italic">
                 Premium Coffee
               </h3>
-
               <p className="text-stone-300 italic">
                 Carefully selected beans roasted to perfection for rich,
                 unforgettable flavor.
@@ -69,24 +64,18 @@ export default function Home() {
             </div>
 
             <div className="bg-[#2a221e] p-8 rounded-2xl hover:-translate-y-2 transition">
-              <div className="text-5xl mb-5"></div>
-
               <h3 className="text-2xl font-bold text-amber-400 mb-4 italic">
                 Fresh Pastries
               </h3>
-
               <p className="text-stone-300 italic">
                 Daily baked pastries made to perfectly complement your coffee.
               </p>
             </div>
 
             <div className="bg-[#2a221e] p-8 rounded-2xl hover:-translate-y-2 transition">
-              <div className="text-5xl mb-5"></div>
-
               <h3 className="text-2xl font-bold text-amber-400 mb-4 italic">
                 Cozy Atmosphere
               </h3>
-
               <p className="text-stone-300 italic">
                 Warm tavern interiors designed for conversations, creativity,
                 and comfort.
@@ -97,16 +86,13 @@ export default function Home() {
       </section>
 
       {/* ABOUT SECTION */}
-
       <section className="px-6 py-24">
         <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-16 items-center">
-          <div>
-            <img
-              src="https://images.unsplash.com/photo-1509042239860-f550ce710b93"
-              alt="Coffee"
-              className="rounded-2xl shadow-2xl"
-            />
-          </div>
+          <img
+            src="https://images.unsplash.com/photo-1509042239860-f550ce710b93"
+            alt="Coffee"
+            className="rounded-xl shadow-2xl"
+          />
 
           <div>
             <p className="uppercase tracking-[0.3em] text-amber-400 mb-3">
@@ -134,18 +120,19 @@ export default function Home() {
       </section>
 
       {/* FOOTER */}
-
-      <footer className="bg-[#211c19] py-10 text-center border-t border-stone-800 h-70 grid-cols-2">
+      <footer className="bg-[#211c19] py-10 text-center border-t border-stone-800 h-54 grid grid-cols-1 md:grid-cols-2">
         <div>
           <h2 className="text-3xl font-bold text-amber-400 mb-3">
             Tavern Coffee
           </h2>
           <p className="text-stone-400">
-            Crafted with warmth • Brewed with passion
+            Crafted with warmth &bull; Brewed with passion
           </p>
         </div>
         <div>
-          <p className="text-stone-400">Contacts</p>
+          <h3 className="mb-2 text-stone-400">Contact Us</h3>
+          <p className="mb-2 text-stone-400">Email: brown&b@example.com</p>
+          <p className="mb-2 text-stone-400">Phone: +254 712 345 678 </p>
         </div>
       </footer>
     </div>
