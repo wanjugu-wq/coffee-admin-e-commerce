979d5971456e7b4406009ee98dd7f527998a76c1
wanjugu-wq <wanjuguzzz@gmail.com>
2026-05-18 21:54:27 +0300

    fixed company name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="bg-[#211c19] shadow-md px-6 py-4 flex justify-between items-center w-full h-25">
       <h1 className="text-2xl font-bold italic text-stone-100 tracking-wider">
-        Tavern Coffee
+        Brown and Bean Co.
       </h1>
 
       <div className="flex gap-6 text-[#2b1d17]">
