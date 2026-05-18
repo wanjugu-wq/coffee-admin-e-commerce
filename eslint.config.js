64d8a2accaf53d91b28d46ab8690407d1fbf97f9
wanjugu-wq <wanjuguzzz@gmail.com>
2026-05-18 23:55:42 +0300

    fixed bugs

--- eslint.config.js
+++ eslint.config.js
@@ -6,16 +6,12 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
+  {files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-    ],
-    languageOptions: {
+    ], languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
-    },
-  },
-])
+    },},])
